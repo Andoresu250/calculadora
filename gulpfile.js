aa5fb88be916ebd9d4842ef801d299c9a80267c2
jestrade <jesuestrada@gmail.com>
2016-08-31 05:36:51 +0000

    bootstrap installed

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,41 @@ var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 
+
 gulp.task('clean',function(){
     return del(['dist/**/*']);
 });
 
-gulp.task('build',['sass','images'],function(){
+gulp.task('build',['sass','images','fonts'],function(){
 });
 
 gulp.task('sass', function () {
   return gulp.src('src/sass/**/*.scss') 
   .pipe(sass({
-      includePaths: require('node-normalize-scss').includePaths
-      
+      outputStyle: 'compressed',
+      includePaths: ['node_modules/bootstrap-sass/assets/stylesheets']
   }))
   .pipe(gulp.dest('dist/css'));
 });
 
+gulp.task('fonts', function () {
+    return gulp
+        .src('node_modules/bootstrap-sass/assets/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'));
+});
  
  gulp.task('images', function(){
   gulp.src('src/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(cache(imagemin({ 
+        optimizationLevel: 3, 
+        progressive: true, 
+        interlaced: true    
+    })))
     .pipe(gulp.dest('dist/images/'));
 });
 
  gulp.task('default',['watch']);
  
- gulp.task('watch', function () {
+ gulp.task('watch', function () { 
   gulp.watch('src/sass/**/*.scss', ['build']);
 });
